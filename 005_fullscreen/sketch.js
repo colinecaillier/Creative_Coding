3f352b302abcc2ac718f88497805022bb9e9040f
colinecaillier <coline.caillier@gmail.com>
2016-10-12 12:10:18 +0200

    color rect - resize height

--- 005_fullscreen/sketch.js
+++ 005_fullscreen/sketch.js
@@ -9,14 +9,19 @@ function setup() {
 
 function draw() {
   mappedX = map(mouseX,0, width,0,255);
-  mappedY= map(mouseY,0,height, 50, 200);
-  background(mappedX);
-  print(mouseX,mouseY);
+  mappedY= map(mouseY,0,height, 0, 150);
+  mappedA = map(mouseX,0,width,0.2,1);
+  //background(mappedX);
+ //print(mappedX,mappedY);
+ print(mappedA);
 
   noStroke();
-  fill(255-mappedX);
-  ellipse(mouseX,mouseY,mappedY,mappedY);
+  fill(255-mappedY,255,255-mappedX);
+  rect(mouseX,mouseY,mappedY,mappedY);
 
+}
 
+function mousePressed(){
+  background(255);
 
 }
