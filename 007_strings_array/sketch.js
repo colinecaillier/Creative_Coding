7a8aa3cd3fc5e4527fc329a40868c67900ef416e
colinecaillier <coline.caillier@gmail.com>
2016-10-19 10:33:10 +0200

    modif

--- 007_strings_array/sketch.js
+++ 007_strings_array/sketch.js
@@ -3,49 +3,81 @@ var words =["in","the","beginning","there","was","the","word"," !"]
 var words2 =["hola","ciao","today","there","was","riaviolis","day"," !"]
 
 var outputText="";
+var offset = 30;
+var counter = 0;
 
 
 function setup() {
   print(words);
   createCanvas(windowWidth,windowHeight);
-  frameRate(5);
+  frameRate(60);
+  textSize(35);
+
+
 
 }
 
 function draw() {
-  background(255);
-  textSize(35);
+  //background(255);
   //text(words[0],100,100);
 
-// shuffle = melanger
-  shuffle(words, true);
+  // shuffle = melanger
+  /*shuffle(words, true);
 
 
   for( var i= 0; i < words.length; i++){
 
-      text(words[i], 100, 100 + i*40);
+    text(words[i], 100, 100 + i*40);
 
   }
+*/
 
+/*
+  for( var j= 0; j < words2.length; j++){
 
-
-  for( var i= 0; i < words2.length; i++){
     textSize(20);
-    outputText += words2[i]+" ";
+    outputText += words2[j]+" ";
 
-    }
-    text(outputText,200,200);
-    outputText = "";
+    text(outputText,200,200+offset);
+    offset +=40;
+    //
 
+  }
+  shuffle(words2, true);
+  outputText = "";
+
+*/
+
+
+counter ++;
+if (counter %60 ==0){
+  print("yeah");
+  doPermutations();
 
 }
 
 
+
+
+
+}
 function mousePressed(){
+  background(255);
+}
+
+
+function doPermutations(){
   print("ici");
+  print(counter);
+  shuffle(words, true);
+  outputText="";
 
+  for ( var i =0; i<words.length; i+=1){
+    outputText = outputText + " " + words[i];
 
-    text(outputText,200+k*40,200);
-    shuffle(words2, true);
+  }
+  text(outputText,100,100+offset);
+  offset += 40;
+
 
 }
